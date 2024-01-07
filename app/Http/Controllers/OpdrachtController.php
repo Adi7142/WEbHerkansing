60896be5c96db3f0337830dce3045d63b1e77276
adi <adi.benh9@gmail.com>
2024-01-07 22:56:10 +0100

    Initial commit

---
+++ app/Http/Controllers/OpdrachtController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Opdracht;
+use App\Models\Fruit;
+
+class OpdrachtController extends Controller
+{
+    public function show()
+    {
+        $name = "Adi Ben Harosh";
+
+        $fruits = Fruit::all();
+
+        // Stuur de opgehaalde Fruits en de $name variabele mee naar de "opdracht" view
+        return view('opdracht', ['fruits' => $fruits, 'name' => $name]);
+    }
+}
